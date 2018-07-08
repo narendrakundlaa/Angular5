6947b0557e302b7165079bced70ac7832a959115
Narendra <kundlaa.narendra@gmail.com>
2018-07-08 23:16:38 +0530

    new button Added

--- src/app/SPA/home.component.ts
+++ src/app/SPA/home.component.ts
@@ -1,10 +1,15 @@
-import {Component } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
-    selector:  'app-home',
+    selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
     public name = 'Narendra';
     public age = 28;
+    public GIT = 'Added this content after GIt';
+    public result = '';
+    public Message(): void {
+       this.result = this.name;
+    }
 }
